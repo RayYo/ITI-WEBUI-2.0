e7d9ada33525e14f6c888166841181235de8ecf8
roy <roy.tsai@iti-tek.com>
2026-07-17 15:13:42 +0800

    [fix] IPv6 Neighbor 过滤行:input 照原版样式(300px/26px/无圆角),默认 * 表示不过滤,Find 后复位 *
    - probe 原版 computed:wrapper 300px 定宽居中、input 26px 高、border #dbdee1、无圆角、padding 0 7px;用页内 .filter-input scoped 类实现(不动全局 .InputInTable,其余 3 页还依赖其 100% 撑格)
    - * 语义:输入 * 或空 = 该列不作为过滤条件;非 * 才参与过滤;Find/首行 Delete 执行后 input 复位 *
    - 过滤行字符白名单放行 *

--- src/views/system/l3-feature/ipv6-neighbor.vue
+++ src/views/system/l3-feature/ipv6-neighbor.vue
@@ -41,13 +41,13 @@
     >
       <el-table-column label="Neighbor IPv6 Address" min-width="33%">
         <template slot-scope="scope">
-          <el-input v-if="scope.row._filter" v-model="fIpv6" class="InputInTable" @input="inputCheck('fIpv6')" />
+          <el-input v-if="scope.row._filter" v-model="fIpv6" class="filter-input" @input="inputCheck('fIpv6')" />
           <span v-else>{{ scope.row.ipv6 }}</span>
         </template>
       </el-table-column>
       <el-table-column label="Link Layer MAC Address" min-width="30%">
         <template slot-scope="scope">
-          <el-input v-if="scope.row._filter" v-model="fMac" class="InputInTable" @input="inputCheck('fMac')" />
+          <el-input v-if="scope.row._filter" v-model="fMac" class="filter-input" @input="inputCheck('fMac')" />
           <span v-else>{{ scope.row.mac }}</span>
         </template>
       </el-table-column>
@@ -98,9 +98,9 @@ export default {
       totalEntry: '',
       loading: false,
       tableData: [],
-      // 过滤行输入(点 Find 时快照到 applied 生效)
-      fIpv6: '',
-      fMac: '',
+      // 过滤行输入:* = 不作为过滤条件(与原版一致),Find 后恢复 *
+      fIpv6: '*',
+      fMac: '*',
       fStatus: '0',
       applied: { ipv6: '', mac: '', status: '0' },
       currentPage: 1,
@@ -139,14 +139,22 @@ export default {
         console.log('l3_ipv6Neighbor get err:', err)
       })
     },
+    filterVal(v) {
+      const t = v.trim()
+      return t === '*' ? '' : t
+    },
     onFind() {
-      this.applied = { ipv6: this.fIpv6.trim(), mac: this.fMac.trim(), status: this.fStatus }
+      this.applied = { ipv6: this.filterVal(this.fIpv6), mac: this.filterVal(this.fMac), status: this.fStatus }
       this.currentPage = 1
+      this.fIpv6 = '*'
+      this.fMac = '*'
     },
     // 首行 Delete:删除当前过滤条件命中的所有表项
     onDeleteFiltered() {
-      this.applied = { ipv6: this.fIpv6.trim(), mac: this.fMac.trim(), status: this.fStatus }
+      this.applied = { ipv6: this.filterVal(this.fIpv6), mac: this.filterVal(this.fMac), status: this.fStatus }
       this.currentPage = 1
+      this.fIpv6 = '*'
+      this.fMac = '*'
       const hits = this.filtered
       if (!hits.length) { message.warnBox('No matched entry.'); return }
       message.confirmWarnBox(`Do you want to delete ${hits.length} matched ${hits.length > 1 ? 'entries' : 'entry'} ?`, 'Please confirm').then(async() => {
@@ -183,9 +191,9 @@ export default {
       if (type === 'ipv6') {
         this.neighborIpv6 = this.neighborIpv6.replace(/[^a-fA-F0-9:]/g, '')
       } else if (type === 'fIpv6') {
-        this.fIpv6 = this.fIpv6.replace(/[^a-fA-F0-9:]/g, '')
+        this.fIpv6 = this.fIpv6.replace(/[^a-fA-F0-9:*]/g, '')
       } else if (type === 'fMac') {
-        this.fMac = this.fMac.replace(/[^a-fA-F0-9:-]/g, '')
+        this.fMac = this.fMac.replace(/[^a-fA-F0-9:*-]/g, '')
       } else {
         this.macAddr = this.macAddr.replace(/[^a-fA-F0-9:-]/g, '')
       }
@@ -193,3 +201,19 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+// 过滤行输入框:照原版 computed(300px 定宽居中、26 高、无圆角、边框 #dbdee1)
+.filter-input {
+  width: 300px !important;
+  max-width: 100%;
+  ::v-deep .el-input__inner {
+    height: 26px;
+    line-height: 26px;
+    padding: 0 7px;
+    color: #606266;
+    border: 1px solid #dbdee1;
+    border-radius: 0;
+  }
+}
+</style>
